d2f44927692c504841d9dcfab8e83ec36e04f1b9
Camilo Londoño Alvarez <cristulxpeke@gmail.com>
2023-04-11 21:23:14 -0500

    ya me dio pero fue por instintio

--- Unidad 1/clase4_Cristian/src/App.js
+++ Unidad 1/clase4_Cristian/src/App.js
@@ -1,58 +1,94 @@
 import "./App.scss";
-import React, {useState} from 'react';
-import Button from "react-bootstrap/Button";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import { Container } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
-
   const [formulario, setFormulario] = useState({
-    email: "",
-    password: ""
+    numero1: "",
+    numero2: "",
   });
 
   const handleChange = (event) => {
-    const temporal = {...formulario}
-    temporal[event.target.name] = event.target.value
-    setFormulario(temporal)
-  }
+    const temporal = { ...formulario }; //copia el objeto que esta dentro de las llaves, quitando referencia
+    temporal[event.target.numero] = event.target.value;
+    setFormulario(temporal);
+  }; //el target.name es como los get y set, y el .value sirve para cambiar el valor
 
   return (
     <div className="App">
       <Container>
         <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              onChange={handleChange}
-              name="email"
-              type="email"
-              placeholder="Enter email"
-            />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+          <Row>
+            <Col>
+              <Form.Group>
+                <Form.Label>Ingrese el numero</Form.Label>
+                <Form.Control
+                  onChange={handleChange}
+                  name="numero1"
+                  type="input"
+                  placeholder="Introducir Numero 1"
+                />
+              </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              onChange={handleChange}
-              name="password"
-              type="password"
-              placeholder="Password"
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          {formulario.email}
-          {formulario.password}
-          <Button variant="primary" type="submit">
+              <Form.Group>
+                <Form.Label>Ingrese el numero</Form.Label>
+                <Form.Control
+                  onChange={handleChange}
+                  name="numero2"
+                  type="input"
+                  placeholder="Introducir Numero 2"
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group>
+                <Form.Label>Resultado Suma</Form.Label>
+                <Form.Control
+                  onChange={handleChange}
+                  name="numero1"
+                  type="input"
+                  placeholder="Suma"
+                />
+              </Form.Group>
 
-            Submit
-          </Button>
+              <Form.Group>
+                <Form.Label>Resultado Resta</Form.Label>
+                <Form.Control
+                  onChange={handleChange}
+                  name="numero1"
+                  type="input"
+                  placeholder="Resta"
+                />
+              </Form.Group>
+
+              <Form.Group>
+                <Form.Label>Resultado Multiplicacion</Form.Label>
+                <Form.Control
+                  onChange={handleChange}
+                  name="numero1"
+                  type="input"
+                  placeholder="Multiplicacion"
+                />
+              </Form.Group>
+
+              <Form.Group>
+                <Form.Label>Resultado Division</Form.Label>
+                <Form.Control
+                  onChange={handleChange}
+                  name="numero1"
+                  type="input"
+                  placeholder="Division"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+
+          {formulario.numero1}
+          {formulario.numero2}
         </Form>
       </Container>
     </div>
