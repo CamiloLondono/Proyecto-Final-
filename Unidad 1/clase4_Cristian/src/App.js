22c3f30a41c3bf1e4e9f74913642f014bafaf18f
Camilo Londoño Alvarez <cristulxpeke@gmail.com>
2023-04-18 19:27:27 -0500

    Ya funciona la calculadora de forma diferente a como la tenia

--- Unidad 1/clase4_Cristian/src/App.js
+++ Unidad 1/clase4_Cristian/src/App.js
@@ -12,11 +12,29 @@ function App() {
     numero2: "",
   });
 
+  const [resultado, setResultado] = useState({
+    suma: 0,
+    resta: 0,
+    multiplicacion: 0,
+    division: 0,
+  })
+
   const handleChange = (event) => {
     const temporal = { ...formulario }; //copia el objeto que esta dentro de las llaves, quitando referencia
-    temporal[event.target.numero] = event.target.value;
+    temporal[event.target.name] = event.target.value;
     setFormulario(temporal);
-  }; //el target.name es como los get y set, y el .value sirve para cambiar el valor
+    //el target.name es como los get y set, y el .value sirve para cambiar el valor
+    const sumaTemp = parseFloat(temporal.numero1) + parseFloat(temporal.numero2)
+    const restaTemp = parseFloat(temporal.numero1) - parseFloat(temporal.numero2)
+    const multiplicacionTemp = parseFloat(temporal.numero1) * parseFloat(temporal.numero2)
+    const divisionTemp = parseFloat(temporal.numero2) !==0 ? parseFloat(temporal.numero1) / parseFloat(temporal.numero2): 'No definido'
+    setResultado({
+      suma: sumaTemp,
+      resta: restaTemp,
+      multiplicacion: multiplicacionTemp,
+      division: divisionTemp
+    })
+  }
 
   return (
     <div className="App">
@@ -33,7 +51,6 @@ function App() {
                   placeholder="Introducir Numero 1"
                 />
               </Form.Group>
-
               <Form.Group>
                 <Form.Label>Ingrese el numero</Form.Label>
                 <Form.Control
@@ -48,47 +65,44 @@ function App() {
               <Form.Group>
                 <Form.Label>Resultado Suma</Form.Label>
                 <Form.Control
-                  onChange={handleChange}
-                  name="numero1"
+                  value={resultado.suma}
+                  name='numero1'
                   type="input"
                   placeholder="Suma"
+                  disabled //comando disabled, es para desibilitar el campo de texto
                 />
               </Form.Group>
-
               <Form.Group>
                 <Form.Label>Resultado Resta</Form.Label>
                 <Form.Control
-                  onChange={handleChange}
-                  name="numero1"
+                  value={resultado.resta}
+                  name='numero1'
                   type="input"
                   placeholder="Resta"
+                  disabled
                 />
               </Form.Group>
-
               <Form.Group>
                 <Form.Label>Resultado Multiplicacion</Form.Label>
                 <Form.Control
-                  onChange={handleChange}
-                  name="numero1"
+                  value={resultado.multiplicacion}
+                  name='numero1'
                   type="input"
                   placeholder="Multiplicacion"
+                  
                 />
               </Form.Group>
-
               <Form.Group>
                 <Form.Label>Resultado Division</Form.Label>
                 <Form.Control
-                  onChange={handleChange}
-                  name="numero1"
+                  value={resultado.division}
                   type="input"
                   placeholder="Division"
+                  disabled
                 />
               </Form.Group>
             </Col>
           </Row>
-
-          {formulario.numero1}
-          {formulario.numero2}
         </Form>
       </Container>
     </div>
