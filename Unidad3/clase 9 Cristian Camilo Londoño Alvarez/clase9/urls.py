f67bcd0055b271fad152e06e5944899bb310507b
Camilo Londoño Alvarez <cristulxpeke@gmail.com>
2023-05-16 21:01:02 -0500

    la clase 10

--- Unidad3/clase 9 Cristian Camilo Londoño Alvarez/clase9/urls.py
+++ Unidad3/clase 9 Cristian Camilo Londoño Alvarez/clase9/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from biblioteca.api.router import router_biblioteca
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
+    #path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
+    path('api/', include(router_biblioteca.urls))
 ]
