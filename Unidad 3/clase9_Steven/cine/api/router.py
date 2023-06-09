ce0c414b1072a2fbcf6ae49dce46b24aeb77bc41
Steven Cordoba <steven.cordoba@correounivalle.edu.co>
2023-06-09 12:45:50 -0500

    he acomodado mis carpetas

--- Unidad 3/clase9_Steven/cine/api/router.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from cine.api.views import CineApiSet
-
-router_cine = DefaultRouter()
-router_cine.register(prefix='cine', basename='cine', viewset=CineApiSet)
