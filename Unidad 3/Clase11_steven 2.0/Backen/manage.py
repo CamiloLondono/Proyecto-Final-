0a4c4a2f7d23bab92e4e4e4538f256cad7e96636
Steven Cordoba <steven.cordoba@correounivalle.edu.co>
2023-06-12 10:43:03 -0500

    solo actualizo x2

--- Unidad 3/Clase11_steven 2.0/Backen/manage.py
+++ Unidad 3/Clase11_steven 2.0/Backen/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clase9_Steven.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clase10.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
